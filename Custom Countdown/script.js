121f270cf0abc9a582dc4c4e10c5d5eae61812b8
3BoOoD97 <abodn70@hotmail.com>
2022-10-09 19:21:02 +0200

    Form Values

--- Custom Countdown/script.js
+++ Custom Countdown/script.js
@@ -1,7 +1,20 @@
 const input = document.getElementById('inputc-container');
-const countDowm = document.getElementById('countdownForm');
+const countDowmForm = document.getElementById('countdownForm');
 const dateEl= document.getElementById('date-picker');
 
+let countDownTitle = '';
+let countDownDate = '';
 //Set Date Input Min with Today's Date
 const todat = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', todat);
+
+// Take Values from Form Input
+function updateCountdown(e) {
+    e.preventDefault();
+    countDownTitle = e.srcElement[0].value;
+    countDownDate = e.srcElement[1].value;
+    console.log(countDownTitle, countDownDate);
+}
+
+//Event Listeners
+countDowmForm.addEventListener('submit', updateCountdown);
